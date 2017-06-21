38b4a1f4731b6b427f8ed7a916d6a2e258d851a8
na57 <na57@qq.com>
2017-06-21 15:56:16 +0800

    update: 修改图标

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { jsonServerRestClient, simpleRestClient, Admin, Resource, Delete } from 'admin-on-rest';
+import { simpleRestClient, Admin, Resource, Delete } from 'admin-on-rest';
 import chineseMessages from 'aor-language-chinese';
+import EmailIcon from 'material-ui/svg-icons/communication/email';
+import WechatOAIcon from 'material-ui/svg-icons/av/art-track';
+import WeiboIcon from 'material-ui/svg-icons/communication/chat';
+import WebsiteIcon from 'material-ui/svg-icons/av/web';
+import DepartmentIcon from 'material-ui/svg-icons/social/people';
+import AqzrIcon from 'material-ui/svg-icons/action/assignment';
+
 import { WebSite, Department, Wechat, Weibo, Email, Dashboard, Aqzr } from './components';
 import authClient from './authClient';
 // import Login from './Login';
@@ -24,22 +31,28 @@ const App = () => (
 
       
       <Resource name="departments" options={{ label: '主管单位' }}
+        icon={DepartmentIcon}
         list={Department.List} create={Department.Create} edit={Department.Edit} remove={Delete}
       />
         <Resource name="websites" options={{ label: '网站及应用系统' }}
+          icon={WebsiteIcon}
           list={WebSite.List} create={WebSite.Create} edit={WebSite.Edit} remove={Delete} show={WebSite.Show}
         />
         <Resource name="persons" />
         <Resource name="wechatOfficialAccounts" options={{ label: '微信公众号' }}
+          icon={WechatOAIcon}
           list={Wechat.List} create={Wechat.Create} edit={Wechat.Edit} remove={Delete}
         />
         <Resource name="weiboAccounts" options={{ label: '微博账号' }}
+          icon={WeiboIcon}
           list={Weibo.List} create={Weibo.Create} edit={Weibo.Edit} remove={Delete}
         />
         <Resource name="emails" options={{ label: '公共电子邮箱' }}
+          icon={EmailIcon}
           list={Email.List} create={Email.Create} edit={Email.Edit} remove={Delete}
         />
         <Resource name="aqzr" options={{ label: '安全责任书' }}
+          icon={AqzrIcon}
           list={Aqzr.List} create={Aqzr.Create} remove={Delete}
         />
     </Admin>
