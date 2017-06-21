4563131cb7f0caaabb7f5ad84066419c8cfb7d5f
na57 <na57@qq.com>
2017-06-21 14:43:59 +0800

    update: 有问题

--- src/components/SelectOrDefaultInput.js
+++ src/components/SelectOrDefaultInput.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { DisabledInput, SelectInput  } from 'admin-on-rest';
+import { DisabledInput, SelectInput, TextInput  } from 'admin-on-rest';
 
 const SelectOrDefaultInput = (props) => {
-    const { choices, input, ...other } = props;
-    if (choices.length === 1) {
-        const props2 = {
-            ...other,
-            input: {
-                ...input,
-                value: props.choices[0][props.optionText],
-            }
-        };
-        return <DisabledInput {...props2} />
-    } else return <SelectInput {...props} />
+    return <SelectInput {...props} />
+    // const { choices, input, ...other } = props;
+    // if (choices.length === 1) {
+    //     const props2 = {
+    //         ...other,
+    //         input: {
+    //             ...input,
+    //             value: props.choices[0][props.optionText],
+    //         }
+    //     };
+    //     return <TextInput {...props2} diabled={true} />
+    // } else return <SelectInput {...props} />
 };
 
 export default SelectOrDefaultInput;
