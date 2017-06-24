40fdf0a9160cbfed55e5d76a138b46a93f5ac523
na57 <na57@qq.com>
2017-06-25 05:55:43 +0800

    update: 修改微博账号页面

--- src/components/weibo-account/Create.js
+++ src/components/weibo-account/Create.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Create, EditButton, ReferenceInput, DateInput, BooleanInput, required, NumberInput, TextInput, TabbedForm, FormTab, SelectInput } from 'admin-on-rest';
-import SelectOrDefaultInput from '../SelectOrDefaultInput';
+import { connect } from 'react-redux';
+import { formValueSelector, change } from 'redux-form';
 
-export default (props) => (
-    <Create {...props} title="添加微博账号信息" >
+class WeiboAccountCreate extends Component {
+    state = {  }
+    componentDidMount() {
+        this.props.change('record-form', 'certification.yrz', false);
+    }
+    render() {
+        const {yrz} = this.props;
+        return (
+            <Create {...this.props} title="添加微博账号信息" >
         <TabbedForm>
             <FormTab label="基本信息">
                 <TextInput source="name" label="名称" validate={[ required ]} />
@@ -17,12 +25,23 @@ export default (props) => (
             <FormTab label="管理员">
                 <TextInput source="manager.name" label="姓名" validate={[ required ]} />
                 <TextInput source="manager.id" label="一卡通号" validate={[ required ]} />                            
-                <NumberInput source="manager.phone" label="手机号" validate={[ required ]} />                
+                <TextInput source="manager.phone" label="手机号" validate={[ required ]} />                
             </FormTab>
             <FormTab label="认证情况">
                 <BooleanInput source="certification.yrz" label="是否已经认证" />
-                <TextInput source="certification.zt" label="认证主体" />              
+                { yrz && <TextInput source="certification.zt" label="认证主体" validate={[ required ]} /> }              
             </FormTab>
         </TabbedForm>
     </Create>
-);
+        );
+    }
+}
+
+
+const selector = formValueSelector('record-form');
+const mapStateToProps = state => ({
+    yrz: selector(state, 'certification.yrz'),
+})
+export default connect(mapStateToProps, {
+    change,
+})(WeiboAccountCreate);
--- src/components/weibo-account/List.js
+++ src/components/weibo-account/List.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { List, Datagrid, TextField, ChipField, BooleanField, ReferenceField, ReferenceArrayField, EditButton, DeleteButton } from 'admin-on-rest';
+import { List, Datagrid, TextField, FunctionField, ChipField, BooleanField, ReferenceField, ReferenceArrayField, EditButton, DeleteButton } from 'admin-on-rest';
+import FlatButton from 'material-ui/FlatButton';
+
+const urlRender = ({url, name}) =>
+    <FlatButton href={url} label={name}
+        labelStyle={{ textTransform: '' }}
+    />;
 
 export default (props) => (
     <List {...props} title="微博账号" >
         <Datagrid>
             <TextField source="name" label="名称" />
-            <TextField source="account" label="账号" />
-            <TextField source="url" label="地址" />
+            <FunctionField label="账号" render={record => urlRender(record)} />
             <ChipField source="manager.name" label="管理员" />
             <BooleanField source="certification.yrz" label="认证情况" />
-            <EditButton />
             <DeleteButton />
         </Datagrid>
     </List>
