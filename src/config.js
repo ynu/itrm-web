854ee18fcfed53b5f235c99e21cdfb180b774b65
na57 <na57@qq.com>
2017-06-27 14:49:47 +0800

    更新默认值

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const apiHost = process.env.API_HOST || 'http://api.itrm.ynu.edu.cn:4000';
+export const apiHost = process.env.API_HOST || 'http://api.itrm.ynu.edu.cn';
 
-export const logoutUrl = process.env.LOGOUT_URL || 'http://api.itrm.ynu.edu.cn:4000/auth/logout';
+export const logoutUrl = process.env.LOGOUT_URL || 'http://api.itrm.ynu.edu.cn/auth/logout';
