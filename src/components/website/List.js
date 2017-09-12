cffdb1de0b4a6e39f9f5a16dd11d662a1b8b8d8f
na57 <na57@qq.com>
2017-09-12 16:12:05 +0800

    update LIst page

--- src/components/website/List.js
+++ src/components/website/List.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { List, Datagrid, TextField, FunctionField, ReferenceField, ChipField, EditButton, DeleteButton } from 'admin-on-rest';
+import { List, Datagrid, TextField, FunctionField, ReferenceField, ChipField, EditButton, ShowButton } from 'admin-on-rest';
 import FlatButton from 'material-ui/FlatButton';
+import { auditStatus } from '../../config';
 
 const urlRender = ({mainPageUrl, name}) =>
     <FlatButton href={mainPageUrl} label={name}
@@ -10,13 +11,28 @@ const urlRender = ({mainPageUrl, name}) =>
 export default (props) => (
     <List {...props} title="网站及应用系统" >
         <Datagrid>
-            <TextField source="domain" label="域名" />
             <FunctionField label="名称" render={record => urlRender(record)} />
-            <ChipField label="管理员" source="manager.name" />
-            <ReferenceField source="dept.id" label="责任单位" reference="zzjg" >
-                <ChipField source="name" />
-            </ReferenceField>
-            <DeleteButton />
+            <TextField source="domain" label="域名" />
+            <TextField source="dept.name" label="责任单位" />
+            <FunctionField label="审核状态"
+              render={record => {
+                if (!record.latestAuditLog) record.latestAuditLog = {
+                  status: auditStatus.CREATED,
+                };
+                switch (record.latestAuditLog.status) {
+                  case auditStatus.CREATED:
+                    return '未提交审核';
+                  case auditStatus.SYDW_APPROVED:
+                    return '已提交审核'
+                  case auditStatus.ITC_APPROVED:
+                    return '信息技术中心已审核通过';
+                  case auditStatus.ITC_REJECTED:
+                    return `信息技术中心审核未通过（原因：${record.latestAuditLog.remark}）`
+                }
+                return '';
+              }}
+            />
+            <ShowButton />
         </Datagrid>
     </List>
 );
